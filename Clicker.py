c50d36c9c722880706a335a6f240ceb16a97f0d0
ASSASSIN <ruslanlatypov913@gmail.com>
2025-05-17 14:13:49 +0300

    New Repositori

--- Clicker.py
+++ Clicker.py
@@ -1,6 +1,6 @@
 from pygame import *
 from random import randint
-from time import sleep
+from time import time as timer
 
 FPS = 60
 clock = time.Clock()
@@ -37,8 +37,42 @@ class Upgrade(GameSprite):
     def update(self):
         pass
 
+class Key(GameSprite):
+    def update(self):
+        pass
+
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+
+class Krest(GameSprite):
+    def update(self):
+        pass
+
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+
+class Lighter(GameSprite):
+    def update(self):
+        pass
+
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+
+class Zashigalka(GameSprite):
+    def update(self):
+        pass
+
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+    
 # Очки
-score = 0
+score = 5000
+score_click = 1
+bot_click = 0
+price_krest = {"krest": 20 }
+price_lighter = {"lighter": 100 }
+price_key = {"key": 300}
+price_zashigalka = {"zashigalka": 5000}
 
 # Текст
 font.init()
@@ -51,10 +85,16 @@ mixer.music.set_volume(0.3)
 mixer.music.play(-1)
 
 # Спрайты
-button = Player("Button.png", 110, 200, 150, 150)
+button = Player("Button.png", 140, 260, 150, 150)
 upgrades = Upgrade("Updates_list.png", 380, 10, 520, 520)
+krest = Krest("Krest.jpg", 530, 90, 80, 230)
+lighter = Lighter("Lighter.jpg", 530, 180, 80, 230)
+key = Key("Key.jpg", 530, 270, 80, 230)
+zashigalka = Zashigalka("Zashigalka.jpg", 530, 360, 80, 230)
+
 
 # Цикл
+start_time = timer()
 game = True
 finish = False
 while game:
@@ -64,22 +104,58 @@ while game:
         if e.type == MOUSEBUTTONDOWN and e.button == 1:
             x, y = e.pos
             if button.collidepoint(x, y):
-                score += 1
-            
+                score += score_click
+            if krest.collidepoint(x, y):
+                if score >= 10:
+                    score_click += 1
+                    score -= price_krest["krest"]
+            if lighter.collidepoint(x, y):
+                if score >= 100:
+                    score_click += 10
+                    score -= price_lighter["lighter"]
+            if key.collidepoint(x, y):
+                if score >= 300 and finish != True:
+                    bot_click += 30
+                    score -= price_key["key"]
+            if zashigalka.collidepoint(x, y):
+                if score >= 5000:
+                    score -= price_zashigalka["zashigalka"]
+                    window.blit(text_WIN, (300, 300))
+
 
 
     if finish != True:
         window.blit(bg, (0, 0))
 
+        
+        now_time = timer()
+        if int(now_time) - int(start_time) == 3:
+            score += bot_click
+            start_time = now_time
+
+
         button.reset()
         upgrades.reset()
+        key.reset()
+        krest.reset()
+        lighter.reset()
+        zashigalka.reset()
 
         button.update()
         upgrades.update()
+        key.update()
+        krest.update()
+        lighter.update()
+        zashigalka.update()
 
         # ОТрисовка текст
         text_score = font1.render("Баллы: " + str(score), 1, (255, 255, 255))
+        text_click = font1.render("За клик: " + str(score_click), 1, (255, 255, 255))
+        text_bot_click = font1.render("За клик бота: " + str(bot_click), 1, (255, 255, 255))
+        text_WIN = font1.render("YOU WIN!!!", (255, 255, 255))
         window.blit(text_score, (10, 20))
+        window.blit(text_click, (10, 70))
+        window.blit(text_bot_click, (10, 120))
 
         display.update()
     clock.tick(FPS)
